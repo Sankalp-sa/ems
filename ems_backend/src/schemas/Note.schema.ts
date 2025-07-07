b06890e09d8319ada5e2440f492adf38071e5fcb
Sankalp-sa <sankalp.p@ahduni.edu.in>
2025-07-07 17:43:13 +0530

    Deleted old schemas

--- ems_backend/src/schemas/Note.schema.ts
+++
@@ -1,9 +0,0 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-
-@Schema()
-export class Note {
-    @Prop({ required: true })
-    note: string;
-}
-
-export const noteSchema = SchemaFactory.createForClass(Note);
